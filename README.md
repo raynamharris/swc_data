c1f4ddc509acbf3dcd514052a575b35b6f665ac4
Rayna Harris <raynamharris@dhcp-146-6-122-176.cns.utexas.edu>
2016-02-09 14:21:26 -0600

    new figures, finalized readme

--- README.md
+++ README.md
@@ -4,18 +4,22 @@ Repo for analyzing SWC data
 # Motivation
 Greg Wilson wrote two blog posts about [active SWC workshops](http://software-carpentry.org/blog/2016/02/active-workshops.html) and [instructor training](http://software-carpentry.org/blog/2016/02/correlations.html). He made some pretty graphs and asked for other plots. 
 
-So, here is my attempt to make some other pretty plots
+So, here is my attempt to make some pretty correlation plots using the instructor training data.
 
-# Online vs In-person teaching
-Here are a few plots show correlations showing how many in the newly trained instructors have taught at least one workshop.
+## Online vs In-person Training Correlation Plots
 
-###This first one is all the data together. 
+![Instructor Training Correlations](instructor-training-stats-1.png)
 
-![In Person vs Online](instructor-training-stats-2.png)
+## How I made these. 
+Check out my [R script](https://github.com/raynamharris/swc_data/blob/master/instructors.R) to see the linear model that I ran to get the R^2 and the commands used to make the plots
 
-###This one is broken down a little bit more for an indepth analysis of the effect of year
+## Plot Description and Interpretation
+### A1 & A2. Not all learners but most badged instructors will teach 
+Just because we train a lot of new instructors doesn't mean that they all go on to teach a workshop. The number of learners is not a great predictor of how many will actually go on to teach a workshop (R^2 = 0.7021), but once they get their badge, they are very likely to teach (R^2 = 0.915)
 
-![In Person vs Online by Year](instructor-training-stats-1.png)
+### B1 & B2. With time, more people will teach a workshop
+We just started the new year, so many newly badged instructors haven't had the chance to teach a workshop. When we account for year, there is a very strong correlation between number of badged instructors and those who have taught a course.  It looks like over time, more and more are teaching.   
 
-###Interpretation
-It looks to me like both in person and online training work equally well if the desired outcome is that new instructors teach at least 1 workshop. 
+
+## C1 & C2. Slightly more badged instructors from in-person training are teaching
+For 2014 and 2015, it looks like a few more badged instructors from the in-person training have gone on to teach a workshop compared to those who completed online training.  Given that the attrition rate for online workshops appears to be greater (not shown), one could conclude that the in-person training is more effective at producing SWC instructors. 
--- instructors.R
+++ instructors.R
@@ -2,49 +2,80 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 library(cowplot)
+require(cowplot) 
 
-#read in data; split date into year, month, day; add rows for 2012 & 2013 so plot colors are same 
+## read in data; split date into year, month, day; add badged instructor column
 instructors <- read.csv("instructor-training-stats.csv", header=TRUE, sep=",")
 instructors.split <- instructors %>% 
   separate(start, c("year", "month", "day"), sep = "\\-") 
+instructors.split <- mutate(instructors.split, badged.instructors = learners - no.badge)
+
+## filter by classroom type, add rows for 2012 & 2013 so plot colors are same 
 inperson <- filter(instructors.split, online < 1)
 online <- filter(instructors.split, online >= 1)
-inperson <- rbind(inperson, c(2012, 1, 1, NA, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ))
-inperson <- rbind(inperson, c(2013, 1, 1, NA, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ))
+inperson <- rbind(inperson, c(2012, 1, 1, NA, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ))
+inperson <- rbind(inperson, c(2013, 1, 1, NA, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ))
 
-#linear model fitting for number of people teaching as function of number people completed and year
-fitinperson <- lm(data = inperson, taught.at.least.once ~ completed.this + year)
-fitonline <- lm(data = online, taught.at.least.once ~ completed.this + year)
+# filter  by year for comparing classroom style
+year2015 <- filter(instructors.split, year==2015)
+year2014<- filter(instructors.split, year==2014)
+
+##linear model fitting to get R^2 for each plot
+fitall.learners <- lm(data = instructors.split, learners ~ completed.this)
+summary(fitall.learners)
+#Multiple R-squared:  0.7021,	Adjusted R-squared:  0.6953 
+fitall.badged <- lm(data = instructors.split, taught.at.least.once ~ badged.instructors)
+summary(fitall.badged)
+#Multiple R-squared:  0.915,	Adjusted R-squared:  0.9131
+fitinperson <- lm(data = inperson, taught.at.least.once ~ badged.instructors + year)
 summary(fitinperson)
+#Multiple R-squared:  0.9637,	Adjusted R-squared:  0.9546 
+fitonline <- lm(data = online, taught.at.least.once ~ badged.instructors + year)
 summary(fitonline)
+#Multiple R-squared:  0.9692,	Adjusted R-squared:  0.9595 
+fit2014 <- lm(data = year2014, taught.at.least.once ~ badged.instructors + online)
+summary(fit2014)
+#Multiple R-squared:  0.9816,	Adjusted R-squared:  0.9755 
+fit2015 <- lm(data = year2015, taught.at.least.once ~ badged.instructors + online)
+summary(fit2015)
+#Multiple R-squared:  0.9234,	Adjusted R-squared:  0.9144
 
-#plots to compare number completed vs taught with year as color code. 
-#o1 and i1 first plots so abline for each year while o2 and i2 do not
-require(cowplot)  
-o1 <- ggplot(online, aes(x=completed.this, y=taught.at.least.once, colour = factor (year))) + 
-  geom_point() + background_grid(major = "xy", minor = "none")  +
-  theme(axis.text.x = element_text(angle=70, vjust=0.5)) +
-  stat_smooth(method = "lm")
-o2 <- ggplot(online, aes(x=completed.this, y=taught.at.least.once, colour = factor (year))) + 
-  geom_point() + background_grid(major = "xy", minor = "none")  +
-  theme(axis.text.x = element_text(angle=70, vjust=0.5)) +
-  stat_smooth(method = "lm", col="Red")
-i1 <- ggplot(inperson, aes(x=completed.this, y=taught.at.least.once, colour = factor (year))) + 
-  geom_point() + background_grid(major = "xy", minor = "none")  +
-  theme(axis.text.x = element_text(angle=70, vjust=0.5)) +
-  stat_smooth(method = "lm")
-i2 <- ggplot(inperson, aes(x=completed.this, y=taught.at.least.once, colour = factor (year))) + 
-  geom_point() + background_grid(major = "xy", minor = "none")  +
-  theme(axis.text.x = element_text(angle=70, vjust=0.5)) +
-  stat_smooth(method = "lm", col="Red")
-plot_grid(o1, i1, o2, i2, labels = c("      online", "      in-person", "      online", "      in-person"))
-ggsave("instructor-training-stats-1.png")
-
-require(cowplot)  
-all1 <- ggplot(instructors.split, aes(x=completed.this, y=taught.at.least.once, colour = factor (online))) + 
+#plot number learers vs number who have taught at least once. color by classroom style
+all<- ggplot(instructors.split, aes(x=learners, y=taught.at.least.once, colour = factor (online))) + 
   geom_point() + background_grid(major = "xy", minor = "none")  +
   theme(axis.text.x = element_text(angle=70, vjust=0.5)) +
   scale_colour_discrete(labels=c("in-person", "online")) +
+  stat_smooth(method = "lm", col="red") 
+#plot number badged instructors (completed.this) vs number who have taught at least once. color by classroom style
+all2<- ggplot(instructors.split, aes(x=badged.instructors, y=taught.at.least.once, colour = factor (online))) + 
+  geom_point() + background_grid(major = "xy", minor = "none")  +
+  theme(axis.text.x = element_text(angle=70, vjust=0.5)) +
+  scale_colour_discrete(labels=c("in-person", "online")) +
+  stat_smooth(method = "lm", col="red") 
+#plot number ONLINE trained badged instructors (completed.this) vs number who have taught at least once. color by year
+online.year <- ggplot(online, aes(x=badged.instructors, y=taught.at.least.once, colour = factor (year))) + 
+  geom_point() + background_grid(major = "xy", minor = "none")  +
+  theme(axis.text.x = element_text(angle=70, vjust=0.5)) +
+  stat_smooth(method = "lm")
+#plot number INPERSON trained badged instructors (completed.this) vs number who have taught at least once. color by year
+inperson.year<- ggplot(inperson, aes(x=badged.instructors, y=taught.at.least.once, colour = factor (year))) + 
+  geom_point() + background_grid(major = "xy", minor = "none")  +
+  theme(axis.text.x = element_text(angle=70, vjust=0.5)) +
   stat_smooth(method = "lm") 
-all1
-ggsave("instructor-training-stats-2.png")
+plotyear2014 <- ggplot(year2014, aes(x=badged.instructors, y=taught.at.least.once, colour = factor (online))) + 
+  geom_point() + background_grid(major = "xy", minor = "none")  +
+  theme(axis.text.x = element_text(angle=70, vjust=0.5)) +
+  stat_smooth(method = "lm")+
+  scale_colour_discrete(labels=c("in-person", "online"))
+plotyear2015 <- ggplot(year2015, aes(x=badged.instructors, y=taught.at.least.once, colour = factor (online))) + 
+  geom_point() + background_grid(major = "xy", minor = "none")  +
+  theme(axis.text.x = element_text(angle=70, vjust=0.5)) +
+  stat_smooth(method = "lm") +
+  scale_colour_discrete(labels=c("in-person", "online"))
+
+#using cowplot to make a grid of plots
+plot_grid(all, all2, online.year, inperson.year, plotyear2014, plotyear2015, ncol=2, 
+          labels = c("    A. all learners", " B. all new instructors", 
+                     "C. online new instructors", "D. in-person new instructors",
+                     "       E. 2014", "        F. 2015"))
+ggsave("instructor-training-stats-1.png")
